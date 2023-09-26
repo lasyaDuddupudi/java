f81a0b22ebd89194275012e2139ee0039737be4f
lasyaDuddupudi <143259036+lasyaDuddupudi@users.noreply.github.com>
2023-09-26 10:05:19 +0530

    Create InterfaceDemo.java

---
+++ InterfaceDemo.java
@@ -0,0 +1,20 @@
+import java.io.*;
+interface Language
+{
+    void getName(String name);
+}
+class ProgrammingLanguage implements Language
+{
+  public void getName(String name)
+    {
+        System.out.println("my favourite programming language is "+name);
+    }
+}
+public class InterfaceDemo
+{
+    public static void main(String args[])
+    {
+        ProgrammingLanguage pl=new ProgrammingLanguage();
+        pl.getName("java");
+    }
+}
